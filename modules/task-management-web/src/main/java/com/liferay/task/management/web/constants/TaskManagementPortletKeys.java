2b906eccb128d91632dda5fa6f0a3a02376f27da
Binh Nguyen Van <nvbinhsoft@gmail.com>
2025-04-10 23:53:31 +0700

    feat: create task management web

---
+++ modules/task-management-web/src/main/java/com/liferay/task/management/web/constants/TaskManagementPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.task.management.web.constants;
+
+/**
+ * @author nvbinhsoft
+ */
+public class TaskManagementPortletKeys {
+
+	public static final String TASKMANAGEMENT =
+		"com_liferay_task_management_web_TaskManagementPortlet";
+
+}
