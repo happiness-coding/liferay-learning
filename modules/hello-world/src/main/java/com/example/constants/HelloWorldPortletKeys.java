6609ee5e12a4e9ab93b545afc26f53d9d0f38b97
Binh Nguyen Van <nvbinhsoft@gmail.com>
2025-04-10 23:08:07 +0700

    feat: init project

---
+++ modules/hello-world/src/main/java/com/example/constants/HelloWorldPortletKeys.java
@@ -0,0 +1,11 @@
+package com.example.constants;
+
+/**
+ * @author nvbinhsoft
+ */
+public class HelloWorldPortletKeys {
+
+	public static final String HELLOWORLD =
+		"com_example_HelloWorldPortlet";
+
+}
