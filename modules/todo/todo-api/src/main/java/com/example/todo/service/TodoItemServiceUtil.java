f020992c2a793615989776d3bd7f3340e80a6433
Binh Nguyen Van <nvbinhsoft@gmail.com>
2025-04-15 14:42:53 +0700

    feat: update service utility to use OSGi ServiceTracker and enhance build configuration

--- modules/todo/todo-api/src/main/java/com/example/todo/service/TodoItemServiceUtil.java
+++ modules/todo/todo-api/src/main/java/com/example/todo/service/TodoItemServiceUtil.java
@@ -5,7 +5,9 @@
 
 package com.example.todo.service;
 
-import com.liferay.portal.kernel.module.service.Snapshot;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.util.tracker.ServiceTracker;
 
 /**
  * Provides the remote service utility for TodoItem. This utility wraps
@@ -21,26 +23,39 @@ import com.liferay.portal.kernel.module.service.Snapshot;
  */
 public class TodoItemServiceUtil {
 
-	/*
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never modify this class directly. Add custom service methods to <code>com.example.todo.service.impl.TodoItemServiceImpl</code> and rerun ServiceBuilder to regenerate this class.
-	 */
+    /*
+     * NOTE FOR DEVELOPERS:
+     *
+     * Never modify this class directly. Add custom service methods to <code>com.example.todo.service.impl.TodoItemServiceImpl</code> and rerun ServiceBuilder to regenerate this class.
+     */
 
-	/**
-	 * Returns the OSGi service identifier.
-	 *
-	 * @return the OSGi service identifier
-	 */
-	public static String getOSGiServiceIdentifier() {
-		return getService().getOSGiServiceIdentifier();
-	}
+    /**
+     * Returns the OSGi service identifier.
+     *
+     * @return the OSGi service identifier
+     */
+    public static String getOSGiServiceIdentifier() {
+        return getService().getOSGiServiceIdentifier();
+    }
 
-	public static TodoItemService getService() {
-		return _serviceSnapshot.get();
-	}
+    public static TodoItemService getService() {
+        return _serviceTracker.getService();
+    }
 
-	private static final Snapshot<TodoItemService> _serviceSnapshot =
-		new Snapshot<>(TodoItemServiceUtil.class, TodoItemService.class);
+    private static final ServiceTracker<TodoItemService, TodoItemService> _serviceTracker;
 
+    static {
+        Bundle bundle = FrameworkUtil.getBundle(TodoItemService.class);
+
+        ServiceTracker<TodoItemService, TodoItemService> serviceTracker = null;
+
+        if (bundle != null) {
+            serviceTracker = new ServiceTracker<TodoItemService, TodoItemService>(
+                bundle.getBundleContext(), TodoItemService.class, null);
+
+            serviceTracker.open();
+        }
+
+        _serviceTracker = serviceTracker;
+    }
 }
